75d107907413ab7574837b600a33f3a6393d871a
HJM <hyy6252@gmail.com>
2026-05-11 11:02:12 +0900

    fix: 배포경로 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/pub-auto-builder/", // ★ 이 줄을 추가하세요 (저장소 이름 양옆에 슬래시 필수!)
 })
